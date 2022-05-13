e9281325ddcd3d67d15a8a81bfccd1db65cdd67e
Lucas Porto <victorlucasporto@gmail.com>
2022-05-12 22:36:12 -0300

    terceira aula completatada

--- main.js
+++ main.js
@@ -19,3 +19,20 @@ function openmenu() {
 function closemenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+//uma vez encontrando um .alguma coisa, quer dizer que é uma propriedade do objeto que apareceu antes.
+
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home,
+#home img,
+.stats,
+#services,
+#services header,
+#services .card,
+#about, about header, about .content`);
