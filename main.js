8f25108b9ce9b6b26f8a4aa422b3b61b388bcdde
godhok <victorlucasporto@gmail.com>
2022-07-15 17:11:49 -0300

    correção do JS com o <juliocesar/>

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // String (textos)
 // Number (numero)
 // Boolean (true | false)
-
+var navigation = document.querySelector("#navigation")
 
 function onScroll() {
+  console.log('julin')
   if (scrollY > 0) {
-      navigation.classList.add('scroll')
-    } else {
-      navigation.classList.remove('scroll')
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
   }
 }
 
 function openmenu() {
   document.body.classList.add('menu-expanded')
-    
 }
 
 function closemenu() {
@@ -22,12 +22,10 @@ function closemenu() {
 
 //uma vez encontrando um .alguma coisa, quer dizer que é uma propriedade do objeto que apareceu antes.
 
-
-
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 700
 }).reveal(`
 #home,
 #home img,
@@ -35,4 +33,4 @@ ScrollReveal({
 #services,
 #services header,
 #services .card,
-#about, about header, about .content`);
+#about, about header, about .content`)
